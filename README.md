6a72c6cd15d988275e480ee2164ae19f6e975df0
fchantrel <fchantrel@pagesjaunes.fr>
2015-03-26 22:12:16 +0100

    Init du projet

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# jeminstalle-io
-Appli spring boot / AngularJS 
+# jeminstalle-io 
+
+This project is an API for jeminstalle.io, it uses Spring Boot.
+
+## Configuration required
+In order to use this project, you need to have Java 8 and Maven installed on your computer.
+
+## Start the app
+To start the application with the embeded server, go into the project location and type :
+```mvn spring-boot:run```
+
+If you want to compile the app, simply use ```mvn package``` as you would have done with any Maven project.
+
+The project uses Spring Loaded so any code change in an existing method will not require app reboot.
+~                                                                                                      
---
+++ src/main/java/io/jeminstalle/App.java
@@ -0,0 +1,26 @@
+package io.jeminstalle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+// Do all the magic
+@EnableAutoConfiguration
+// Enable Component Scanning
+@ComponentScan
+public class App
+{
+        /**
+         * Main function to start my app
+         * @param args Arguments given on launch
+         * @throws Exception If something bad happens
+         */
+    public static void main(String[] args) throws Exception {
+
+        // Simply starts my webapp
+        SpringApplication.run(App.class, args);
+
+        System.out.println("Hello Spring Boot!");
+    }
+}
+
