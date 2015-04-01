09198771719493825850584e84571d430468c0d1
Raphael DELAPORTE <raphael.delaporte@ellixo.com>
2015-04-01 10:40:27 +0200

    nouveau controlle par latitude/longitude

--- src/main/java/io/jeminstalle/service/impl/DataParticulierServiceImpl.java
+++ src/main/java/io/jeminstalle/service/impl/DataParticulierServiceImpl.java
@@ -40,26 +40,50 @@ public class DataParticulierServiceImpl implements DataParticulierService {
     @Override
     public DataParticulier getDataParticulierByName(String name) {
 
-        DataParticulier dp = new DataParticulier();
-
         RefGeo refGeo = refGeoDao.findByName(name).get(0);
+        DataParticulier dp = getDataParticulier(refGeo, name, refGeo.getLatitude(), refGeo.getLongitude());
+
+        return dp;
+    }
+
+
+    @Override
+    public DataParticulier getDataParticulierByPosition(String latitude, String longitude) {
+
+        RefGeo refGeo = refGeoDao.findByLatitudeAndLongitude(Float.valueOf(latitude), Float.valueOf(longitude)).get(0);
+        DataParticulier dp = getDataParticulier(refGeo, refGeo.getName(), refGeo.getLatitude(), refGeo.getLongitude());
+
+        return dp;
+    }
+
+
+    private DataParticulier getDataParticulier(RefGeo refGeo, String commune, float latitude, float longitude) {
 
         String departement = refGeo.getZipcode().substring(0, 2);
+
+        DataParticulier dp = new DataParticulier();
+
         Pollution pollution = pollutionDAO.findByNodepartement(departement).get(0);
         Couverture4G couverture4G = couverture4GDAO.findByCodedepartement(departement);
         Ensoleillement ensoleillement = ensoleillementDAO.findByNodepartement(departement);
-        RevenuMoyen revenuMoyen = revenuMoyenDAO.findByNomcommune(name).get(0);
+        RevenuMoyen revenuMoyen = revenuMoyenDAO.findByNomcommune(commune).get(0);
         Precipitation precipitation = precipitationDAO.findByNodepartement(departement);
         Nucleaire nucleaire = nucleaireDAO.findByNumdep(departement);
+
+        dp.setPollution(pollution);
+        dp.setCouverture4G(couverture4G);
+        dp.setRevenuMoyen(revenuMoyen);
+        dp.setPrecipitation(precipitation);
+        dp.setNucleaire(nucleaire);
 
 
         String distanceKM = "12";
         String maxResultat = "10";
 
-        String boulangeriesJSON = proDAO.findByCoordonneesAndRubrique(String.valueOf(refGeo.getLatitude()), String.valueOf(refGeo.getLongitude()), "boulangerie", distanceKM, maxResultat);
-        String pharmacieJSON = proDAO.findByCoordonneesAndRubrique(String.valueOf(refGeo.getLatitude()), String.valueOf(refGeo.getLongitude()), "pharmacie", distanceKM, maxResultat);
-        String barJSON = proDAO.findByCoordonneesAndRubrique(String.valueOf(refGeo.getLatitude()), String.valueOf(refGeo.getLongitude()), "bar", distanceKM, maxResultat);
-        String ecoleJSON = proDAO.findByCoordonneesAndRubrique(String.valueOf(refGeo.getLatitude()), String.valueOf(refGeo.getLongitude()), "ecole primaire", distanceKM, maxResultat);
+        String boulangeriesJSON = proDAO.findByCoordonneesAndRubrique(String.valueOf(latitude), String.valueOf(longitude), "boulangerie", distanceKM, maxResultat);
+        String pharmacieJSON = proDAO.findByCoordonneesAndRubrique(String.valueOf(latitude), String.valueOf(longitude), "pharmacie", distanceKM, maxResultat);
+        String barJSON = proDAO.findByCoordonneesAndRubrique(String.valueOf(latitude), String.valueOf(longitude), "bar", distanceKM, maxResultat);
+        String ecoleJSON = proDAO.findByCoordonneesAndRubrique(String.valueOf(latitude), String.valueOf(longitude), "ecole primaire", distanceKM, maxResultat);
 
 
         dp.setBoulangeries(boulangeriesJSON);
@@ -67,14 +91,9 @@ public class DataParticulierServiceImpl implements DataParticulierService {
         dp.setBars(barJSON);
         dp.setEcoles(ecoleJSON);
 
-
         dp.setRefGeo(refGeo);
-        dp.setPollution(pollution);
-        dp.setCouverture4G(couverture4G);
-        dp.setRevenuMoyen(revenuMoyen);
-        dp.setPrecipitation(precipitation);
-        dp.setNucleaire(nucleaire);
 
         return dp;
+
     }
 }
