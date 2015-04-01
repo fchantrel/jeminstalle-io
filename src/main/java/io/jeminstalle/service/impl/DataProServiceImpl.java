d99fab6d5a80a3836a9dcaf06bcefb571fe24e43
fchantrel <fchantrel@pagesjaunes.fr>
2015-04-01 10:06:14 +0200

    Enrichissement des pros et ajout du filtre CORS.

--- src/main/java/io/jeminstalle/service/impl/DataProServiceImpl.java
+++ src/main/java/io/jeminstalle/service/impl/DataProServiceImpl.java
@@ -3,6 +3,7 @@ package io.jeminstalle.service.impl;
 import io.jeminstalle.dao.*;
 import io.jeminstalle.domain.*;
 import io.jeminstalle.service.DataProService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -74,6 +75,84 @@ public class DataProServiceImpl implements DataProService {
 */
 
         //proDAO.findByCoordonneesAndRubrique(String.valueOf(refGeo.getLatitude()), String.valueOf(refGeo.getLongitude()), "restaurant");
+
+        return dp;
+    }
+
+	@Override
+	    public DataPro getDataProByRegion(String activite, String ou) {
+	
+	        DataPro dp = new DataPro();
+	        dp.setActivite(activite);
+	        dp.setOu(ou);
+	        
+	        try{
+	        	List<LightRefGeo> lstRefGeo = lightRefGeoDao.findByInseecode(ou);
+	        	if(lstRefGeo.size() > 0){
+	        		LightRefGeo refGeo = lstRefGeo.get(0);
+	        		dp.setPopulation(refGeo.getPopulation());
+	        	} else {
+	        		System.out.println("Pas de lieu trouve pour le code insee : " + ou);
+	        		dp.setPopulation(10000);
+	        	}
+	            
+	            dp.setNbPro(10);
+	            dp.setRatio(1.123);
+	        } catch(Exception e){
+	        	e.printStackTrace();
+	        }
+	
+	        return dp;
+	    }
+	
+	@Override
+    public DataPro getDataProByDepartement(String activite, String ou) {
+
+        DataPro dp = new DataPro();
+        dp.setActivite(activite);
+        dp.setOu(ou);
+        
+        try{
+        	List<LightRefGeo> lstRefGeo = lightRefGeoDao.findByInseecode(ou);
+        	if(lstRefGeo.size() > 0){
+        		LightRefGeo refGeo = lstRefGeo.get(0);
+        		dp.setPopulation(refGeo.getPopulation());
+        	} else {
+        		System.out.println("Pas de lieu trouve pour le code insee : " + ou);
+        		dp.setPopulation(10000);
+        	}
+            
+            dp.setNbPro(10);
+            dp.setRatio(1.123);
+        } catch(Exception e){
+        	e.printStackTrace();
+        }
+
+        return dp;
+    }
+	
+	@Override
+    public DataPro getDataProByCommune(String activite, String ou) {
+
+        DataPro dp = new DataPro();
+        dp.setActivite(activite);
+        dp.setOu(ou);
+        
+        try{
+        	List<LightRefGeo> lstRefGeo = lightRefGeoDao.findByInseecode(ou);
+        	if(lstRefGeo.size() > 0){
+        		LightRefGeo refGeo = lstRefGeo.get(0);
+        		dp.setPopulation(refGeo.getPopulation());
+        	} else {
+        		System.out.println("Pas de lieu trouve pour le code insee : " + ou);
+        		dp.setPopulation(10000);
+        	}
+            
+            dp.setNbPro(10);
+            dp.setRatio(1.123);
+        } catch(Exception e){
+        	e.printStackTrace();
+        }
 
         return dp;
     }
