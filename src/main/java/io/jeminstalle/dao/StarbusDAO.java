a10580f63b10a383164df690690ee14fddebb652
fchantrel <fchantrel@pagesjaunes.fr>
2015-04-15 22:24:28 +0200

    Métriques aussi sur ce DAO.

--- src/main/java/io/jeminstalle/dao/StarbusDAO.java
+++ src/main/java/io/jeminstalle/dao/StarbusDAO.java
@@ -4,8 +4,11 @@ import io.jeminstalle.domain.Starbus;
 
 import java.util.List;
 
+import com.codahale.metrics.annotation.Timed;
+
 public interface StarbusDAO {
 
+	@Timed
     List<Starbus> findByLatitudeAndLongitude(double latitude, double longitude, String amplitude);
 
 }
